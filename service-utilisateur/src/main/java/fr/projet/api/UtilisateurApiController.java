508bb9945838cad1f0e1bd5b2c0c99fa2fca223f
sawsan <bastaoui.saousane@gmail.com>
2024-06-05 20:54:21 +0200

    modification Openfeign

--- service-utilisateur/src/main/java/fr/projet/api/UtilisateurApiController.java
+++ service-utilisateur/src/main/java/fr/projet/api/UtilisateurApiController.java
@@ -1,10 +1,13 @@
 package fr.projet.api;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,21 +16,50 @@ import org.springframework.web.server.ResponseStatusException;
 
 import fr.projet.api.dto.ConnexionDTO;
 import fr.projet.api.dto.InscriptionDTO;
+import fr.projet.feignClient.CompteFeignClient;
 import fr.projet.model.Utilisateur;
 import fr.projet.repository.UtilisateurRepository;
+import fr.projet.response.UtilisateurResponse;
 import fr.projet.api.dto.ConnexionDTO;
 
 
 @RestController
 @RequestMapping("api/utilisateur")
 public class UtilisateurApiController {
+
 @Autowired 
  private UtilisateurRepository utilisateurRepository;
+ @Autowired
+ private UtilisateurResponse utilisateurResponse;
 
 @Autowired
-private CompteFeignClient CompteFeignClient;
+private CompteFeignClient compteFeignClient;
 
-   
+@GetMapping
+public List<UtilisateurResponse> findAll() {
+    List<Utilisateur> utilisateurs = this.utilisateurRepository.findAll();
+    List<UtilisateurResponse> response = new ArrayList<>();
+
+    for (Utilisateur utilisateur : utilisateurs) {
+        UtilisateurResponse utilisateurResponse = new UtilisateurResponse();
+
+        BeanUtils.copyProperties(utilisateur, utilisateurResponse);
+
+        String idCompte = this.compteFeignClient.getComptesByUtilisateurId(utilisateur.getId());
+        
+        if (idCompte != null) {
+            utilisateurResponse.setIdCompte(idCompte);
+        }
+
+        response.add(utilisateurResponse);
+    }
+    
+    return response;
+}
+
+
+
+
 @PostMapping("/connexion")
 	public Utilisateur connexion(@RequestBody ConnexionDTO connexionDTO) {
 		Optional<Utilisateur> optUtilisateur = this.utilisateurRepository.findByEmailAndPassword(connexionDTO.getEmail(), connexionDTO.getPassword());
@@ -50,6 +82,12 @@ private CompteFeignClient CompteFeignClient;
 
 		return utilisateur;
 	}
+
+
+
+
+
+
 
 
 }
