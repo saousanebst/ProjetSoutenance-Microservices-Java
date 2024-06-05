bc7026e577e21eaf1391ea45b5bd8540b1351ae8
sawsan <bastaoui.saousane@gmail.com>
2024-06-05 15:10:07 +0200

    en cours
    connexion+Inswription+matchDesServices

--- service-compte/src/main/java/fr/projet/service/CompteSrv.java
+++ service-compte/src/main/java/fr/projet/service/CompteSrv.java
@@ -1,7 +1,6 @@
 package fr.projet.service;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
