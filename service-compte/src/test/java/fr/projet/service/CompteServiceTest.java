1fd717e055b8830cf65a1a0023bd6598a4dae845
sawsan <bastaoui.saousane@gmail.com>
2024-07-06 17:02:59 +0200

    test Compte Modification
    succes

--- service-compte/src/test/java/fr/projet/service/CompteServiceTest.java
+++ service-compte/src/test/java/fr/projet/service/CompteServiceTest.java
@@ -2,7 +2,6 @@ package fr.projet.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Answers.values;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
