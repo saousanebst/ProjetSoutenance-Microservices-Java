783fc839fbc0e528c272e7b51b1d2541969e8246
sawsan <bastaoui.saousane@gmail.com>
2024-07-08 14:36:45 +0200

    test compte et note
    succes

--- service-compte/src/test/java/fr/projet/ServiceCompteApplicationTests.java
+++ service-compte/src/test/java/fr/projet/ServiceCompteApplicationTests.java
@@ -17,7 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import fr.projet.service.CryptographService;
 
-@SpringBootTest
+
 class ServiceCompteApplicationTests {
 @Autowired
 
